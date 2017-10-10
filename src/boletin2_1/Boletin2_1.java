0976e0c94fd1ad910cde3f10eac77ad4041ce1bc
lrodriguezbarroncas <lrodriguezbarroncas@victoria7.danielcastelao.org>
2017-10-10 08:51:57 +0200

    Boletin 2_1

---
+++ src/boletin2_1/Boletin2_1.java
@@ -0,0 +1,16 @@
+
+package boletin2_1;
+
+
+public class Boletin2_1 {
+
+
+    public static void main(String[] args) {
+        float base=4.00F, altura=3.00F, area;
+        area = base*altura/2;
+        System.out.println("area= "+area);
+
+       
+    }
+    
+}
